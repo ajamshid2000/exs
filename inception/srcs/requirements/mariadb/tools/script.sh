a460104540cc32015152a81fa04148a156f0506a
ajamshid <rashid_jamshedi@yahoo.com>
2025-09-14 00:42:14 +0200

    all

--- inception/srcs/requirements/mariadb/tools/script.sh
+++ inception/srcs/requirements/mariadb/tools/script.sh
@@ -1,13 +1,24 @@
+# sleep 10
+set -ex
+echo ">>> Running MariaDB init script..."
 
-echo ">>> Running MariaDB init script...
+mysqld &
+
+until mysqladmin ping --silent; do
+    sleep 1
+done
+
+echo ">>> after running mysqld"
 
 mysql -u root -e "CREATE DATABASE IF NOT EXISTS ${mysql_database};"
-mysql -u root -e "CREATE USER IF NOT EXISTS '${mysql_user}' IDENTIFIED BY '${pass}';"
-mysql -u root -e "GRANT ALL PRIVILEGES ON ${mysql_database}.* TO ${mysql_user};"
-mysql -u "root" -p${mysql_root_password} -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${mysql_pass}';"
-mysql -u root -e "FLUSH PRIVILEGES;"
+# mysql -u root -e "CREATE USER IF NOT EXISTS '${mysql_user}' IDENTIFIED BY '${pass}';"
+# mysql -u root -e "GRANT ALL PRIVILEGES ON ${mysql_database}.* TO ${mysql_user};"
+# mysql -u "root" -p${mysql_root_password} -e "ALTER USER 'root'@'localhost' IDENTIFIED BY '${mysql_pass}';"
+# mysql -u root -e "FLUSH PRIVILEGES;"
 
-mysqladmin -u root -p"${mysql_pass}" shutdown
-
+mysqladmin shutdown
+# mysqladmin -u root -p"${mysql_pass}" shutdown
+echo ">>> ending MariaDB init script..."
 # Wait for shutdown
-wait "$pid"
+# wait "$pid"
+exec "$@"
